6904d636ca0a3b8c75fd12871f57de3c8c344186
EC2 Default User <ec2-user@ip-172-31-24-196.ap-northeast-1.compute.internal>
2022-11-28 13:16:38 +0000

    forth commit

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,2 +1,12 @@
-class Public::RegistrationsController < ApplicationController
+class Public::RegistrationsController < Devise::RegistrationsController
+  before_action :authenticate_customer!, except: [:top, :about]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name,
+    :last_name_kana, :first_name_kana, :email, :encrypted_password,
+    :postal_code, :address, :telephone_number, :is_deleted])
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :customers
-  devise_for :admins
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   scope module: :public do
     root to: 'homes#top'
     get 'homes/about'=> "homes#about", as:'about'
