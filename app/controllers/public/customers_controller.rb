dcf073e47bf878c9c7bc1243e9d1dc428b2da9da
EC2 Default User <ec2-user@ip-172-31-24-196.ap-northeast-1.compute.internal>
2022-11-29 01:20:47 +0000

    fifth commit

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,34 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+
+  def show
+     @customer = current_customer
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer= Customer.find(params[:id])
+    if @customer.update(client_params)
+      flash[:success] = "個人情報を編集しました"
+      redirect_to customer_path(current_customer.id)
+    else
+      flash[:danger] = '個人情報の編集に失敗しました'
+      render :edit
+    end
+  end
+
+  def destroy
+     @customer = Customer.find(params[:id])
+     @customer.destroy
+    redirect_to '/'
+  end
 end
+
+private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :telephone_number, :postal_code, :address)
+  end
